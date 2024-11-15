096339304d096922cc3ce9294b58912880bb2765
Lukas Vallejo Meluk <emlukas82@hotmail.com>
2024-11-15 17:36:01 +0100

    Ajuste index.js

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 
 dotenv.config();
 
@@ -11,9 +12,17 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-// Rutas
+// Ruta para archivos estáticos (sirve el frontend)
+app.use(express.static(path.join(__dirname, '../test/front-test'))); // Asegúrate de que esta ruta es correcta
+
+// Rutas de la API
 const agreementRoutes = require('./routes/agreements.routes');
 app.use('/api/agreements', agreementRoutes);
+
+// Ruta para servir index.html al acceder a la raíz del dominio
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../test/front-test/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`API running on http://localhost:${port}`);
